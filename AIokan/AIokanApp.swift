71f5de37566ca66ccbd45e75f2644d4f87e65e53
zonehisa <nijipapazoneokun123@gmail.com>
2025-02-26 09:06:20 +0900

    Initial Commit

---
+++ AIokan/AIokanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIokanApp.swift
+//  AIokan
+//
+//  Created by 佐藤幸久 on 2025/02/26.
+//
+
+import SwiftUI
+
+@main
+struct AIokanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIokan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIokan
+//
+//  Created by 佐藤幸久 on 2025/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
